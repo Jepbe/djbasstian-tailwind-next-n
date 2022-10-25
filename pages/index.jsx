5e6c9da1a8aa4775117f9b4bb432b1ca824db4bc
Jeppe B. Bech <67188645+Jepbe@users.noreply.github.com>
2022-10-25 23:57:43 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,10 @@ export default function App() {
     <>
       <Head>
         <title>Template</title>
+        <meta 
+          property='og:image'
+          content='https://djbasstian-tailwind-next-n.vercel.app/api/og'
+        />
         <link rel="icon" href="/BASS64x64Hvid.ico" />
       </Head>
       <div className='w-screen h-screen bg-forsideBg2 mainBG bg-cover'>
