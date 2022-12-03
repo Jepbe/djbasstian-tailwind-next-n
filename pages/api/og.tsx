c9998751e7d6b15c40493ad1327c50481b6369cd
Jeppe B. Bech <67188645+Jepbe@users.noreply.github.com>
2022-12-03 18:42:53 +0100

    9

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,30 +1,109 @@
+// import { ImageResponse } from '@vercel/og';
+// import { bassLogo } from '/public/djBasstianOriginal.png';
+ 
+// export const config = {
+//   runtime: 'experimental-edge',
+// };
+
+// export default function Og() {
+//   return new ImageResponse(
+//     (
+//       <div
+//         style={{
+//           fontSize: 128,
+//           background: 'white',
+//           width: '100%',
+//           height: '100%',
+//           display: 'flex',
+//           textAlign: 'center',
+//           alignItems: 'center',
+//           justifyContent: 'center',
+//         }}
+//       >
+//         qwe
+//       </div>
+//     ),
+//     {
+//       width: 1200,
+//       height: 630,
+//     },
+//   );
+// }
+
 import { ImageResponse } from '@vercel/og';
+import { NextRequest } from 'next/server';
 
 export const config = {
   runtime: 'experimental-edge',
 };
 
-export default function Og() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          textAlign: 'center',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        DJ BASStian
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    },
-  );
+export default function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    // ?title=<title>
+    const hasTitle = searchParams.has('title');
+    const title = hasTitle
+      ? searchParams.get('title')?.slice(0, 100)
+      : 'My default title';
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: 'black',
+            backgroundSize: '150px 150px',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              justifyItems: 'center',
+            }}
+          >
+            <img
+              alt="Vercel"
+              height={200}
+              src="http://localhost:3000/djBasstianOriginal.png"
+              style={{ margin: '0 30px' }}
+              width={232}
+            />
+          </div>
+          <div
+            style={{
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              color: 'white',
+              marginTop: 30,
+              padding: '0 120px',
+              lineHeight: 1.4,
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {title}
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
